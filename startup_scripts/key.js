f069f3c7ddf3908152713993104b2c25daae0ae6
Cloverine-Hou <nm305121213@outlook.com>
2026-08-24 12:07:54 +0800

    Auto-sync: 12:07:54

--- startup_scripts/key.js
+++ startup_scripts/key.js
@@ -1,6 +1,7 @@
 StartupEvents.init(() => {
     const $KeyMapping = Java.loadClass("net.minecraft.client.KeyMapping")
     const $GLFWkey = Java.loadClass("org.lwjgl.glfw.GLFW")
+    const $KeyMappingRegistry = Java.loadClass("dev.architectury.registry.client.keymappings.KeyMappingRegistry")
     global.pearlKey = new $KeyMapping("key.kubejs.pearl", $GLFWkey.GLFW_KEY_Y, "key.keybinding.kubejs")
     $KeyMappingRegistry.register(global.pearlKey)
 })
