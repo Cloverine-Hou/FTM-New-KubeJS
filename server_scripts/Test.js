e162b993933e8dba5df547c8fc7f467b3ec2a2b2
Cloverine-Hou <nm305121213@outlook.com>
2026-08-24 11:36:10 +0800

    Auto-sync: 11:36:10

--- server_scripts/Test.js
+++ server_scripts/Test.js
@@ -1,1 +1,10 @@
-ServerEvents
+ServerEvents.recipes(event => {
+    event.shaped("3x minecraft:stone", [
+		'DDD',
+		'   ',
+		'W W'
+	], {
+		D: 'minecraft:diamond',
+		W: 'minecraft:white_wool'
+    });
+});
