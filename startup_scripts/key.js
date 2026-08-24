151cb5386b7834e1be7e94664930dfb792b9cd30
Cloverine-Hou <nm305121213@outlook.com>
2026-08-24 11:57:20 +0800

    Auto-sync: 11:57:20

--- startup_scripts/key.js
+++ startup_scripts/key.js
@@ -1,4 +1,5 @@
 StartupEvents.init(() => {
+    const $GLFWkey = Java.loadClass("org.lwjgl.glfw.GLFW")
     global.pearlKey = new $KeyMapping("key.kubejs.pearl", $GLFWkey.GLFW_KEY_Y, "key.keybinding.kubejs")
     $KeyMappingRegistry.register(global.pearlKey)
 })
